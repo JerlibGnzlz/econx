7220129613982c35a05cd180a9333ea1fe3982a5
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-03-02 03:41:58 -0300

    fix session

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    // ❌ Elimina esta línea, ya no es necesaria
-    serverActions: true,
-  },
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     // ❌ Elimina esta línea, ya no es necesaria
+//     serverActions: true,
+//     runtime: "nodejs",
+//   },
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+// @ts-check
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// }
+
+// module.exports = nextConfig
+
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  /* config options here */
+}
+
+export default nextConfig
